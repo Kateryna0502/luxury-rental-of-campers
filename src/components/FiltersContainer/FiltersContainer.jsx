15ce2ce61f8b02fb6af869478a8b7698c51cea91
Kateryna <mweoliss74@gmail.com>
2024-12-21 21:05:57 +0200

    fixed

--- src/components/FiltersContainer/FiltersContainer.jsx
+++ src/components/FiltersContainer/FiltersContainer.jsx
@@ -1,40 +1,40 @@
 
 
-import { useState } from 'react';
-import FilterPanel from '../FilterPanel/FilterPanel.jsx';
+// import { useState } from 'react';
+// import FilterPanel from '../FilterPanel/FilterPanel.jsx';
 
-const FiltersContainer = () => {
-  const [filters, setFilters] = useState([
-    { id: 'ac', name: 'AC', iconId: 'ac', type: 'checkbox', checked: false },
-    { id: 'automatic', name: 'Automatic', iconId: 'automatic', type: 'checkbox', checked: false },
-    { id: 'kitchen', name: 'Kitchen', iconId: 'kitchen', type: 'checkbox', checked: false },
-    { id: 'tv', name: 'TV', iconId: 'tv', type: 'checkbox', checked: false },
-    { id: 'bathroom', name: 'Bathroom', iconId: 'bathroom', type: 'checkbox', checked: false },
-  ]);
+// const FiltersContainer = () => {
+//   const [filters, setFilters] = useState([
+//     { id: 'ac', name: 'AC', iconId: 'ac', type: 'checkbox', checked: false },
+//     { id: 'automatic', name: 'Automatic', iconId: 'automatic', type: 'checkbox', checked: false },
+//     { id: 'kitchen', name: 'Kitchen', iconId: 'kitchen', type: 'checkbox', checked: false },
+//     { id: 'tv', name: 'TV', iconId: 'tv', type: 'checkbox', checked: false },
+//     { id: 'bathroom', name: 'Bathroom', iconId: 'bathroom', type: 'checkbox', checked: false },
+//   ]);
 
-  const handleChange = (id) => {
-    setFilters((prevFilters) =>
-      prevFilters.map((filter) =>
-        filter.id === id ? { ...filter, checked: !filter.checked } : filter
-      )
-    );
-  };
+//   const handleChange = (id) => {
+//     setFilters((prevFilters) =>
+//       prevFilters.map((filter) =>
+//         filter.id === id ? { ...filter, checked: !filter.checked } : filter
+//       )
+//     );
+//   };
 
-  return (
-    <div>
-      {filters.map((filter) => (
-        <FilterPanel
-          key={filter.id}
-          id={filter.id}
-          name={filter.name}
-          iconId={filter.iconId}
-          type={filter.type}
-          checked={filter.checked}
-          onChange={() => handleChange(filter.id)}
-        />
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {filters.map((filter) => (
+//         <FilterPanel
+//           key={filter.id}
+//           id={filter.id}
+//           name={filter.name}
+//           iconId={filter.iconId}
+//           type={filter.type}
+//           checked={filter.checked}
+//           onChange={() => handleChange(filter.id)}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
 
-export default FiltersContainer;
+// export default FiltersContainer;
--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -134,18 +134,3 @@ export default CamperInfoPage;
 
 
 
-// import css from './CatalogPage.module.css';
-// import FilterPanel from '../../components/FilterPanel/FilterPanel.jsx';
-// import Section from '../../components/Section/Section.jsx';
-
-
-
-// const CatalogPage = () => {
-//   return (
-//     <Section className={css.section}>
-//       <FilterPanel />
-//     </Section>
-//   );
-// };
-
-// export default CatalogPage;
